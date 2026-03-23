40420c565a891b890747693be4fac7965a2f5ec7
Danel <danel.rueda@opendeusto.es>
2026-03-23 15:09:32 +0100

    mejora en registro

---
+++ padelplay-server/src/main/java/com/padelplay/server/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.padelplay.server.repository;
+
+import com.padelplay.server.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    boolean existsByEmail(String email);
+}
